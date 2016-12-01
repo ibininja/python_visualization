5cbbf890d570c92a40b1c74d0fbbec64d69df1b0
ibininja <ibrahim.mokdad@gmail.com>
2016-12-01 16:13:13 +0300

    code for adding multiple plots in a  graph with selection enabled

---
+++ 2-multiple_graphs_with_selection.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thr Dec 1 13:22:37 2016
+
+@author: Ibrahim
+"""
+ 
+from bokeh.plotting import figure
+from bokeh.io import output_file, show
+
+fg = figure(x_axis_label="x_axis", y_axis_label="y_axis", 
+            tools='box_select,lasso_select')
+
+x=[x for x in range(20)]
+y=[y for y in range (23)]
+
+ 
+fg.line(x,y, line_width=4)
+fg.circle(x,y, size=5, fill_color='red')
+
+
+output_file('sample_plot.html')
+show(fg)
